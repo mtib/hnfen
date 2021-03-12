2b5267b60a69a4c2c0780e8da0cafe62c48cef7f
Markus Becker <mtib.becker@gmail.com>
2021-03-12 19:24:47 +0100

    BImproved error handling

--- src/moves.rs
+++ src/moves.rs
@@ -1,8 +1,10 @@
 use std::fmt::Display;
 
-use crate::types::{Board, Piece, Player};
+use crate::types::{Board, Hnfen, Piece};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Copy, Clone, Hash)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Position {
     column: char,
     rank: u8,
@@ -31,19 +33,40 @@ impl Position {
     }
 }
 
-#[derive(Debug, Clone, Hash)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Move {
-    from: Position,
-    to: Position,
+    pub from: Position,
+    pub to: Position,
 }
 
 impl Display for Move {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}{}", self.from, self.to)
+        write!(f, "{}", self.as_hnfen())
     }
 }
 
-enum Direction {
+impl Hnfen for Move {
+    fn as_hnfen(&self) -> String {
+        format!("{}{}", self.from, self.to)
+    }
+
+    fn from_hnfen(hnfen: &str) -> Option<Self> {
+        let move_re = Regex::new(r"^([a-k])(\d{1,2})([a-k])(\d{1,2})$").unwrap();
+        let cap = move_re.captures(hnfen).unwrap();
+        Some(Move {
+            from: Position {
+                column: cap.get(1)?.as_str().chars().next()?,
+                rank: cap.get(2)?.as_str().parse().ok()?,
+            },
+            to: Position {
+                column: cap.get(3)?.as_str().chars().next()?,
+                rank: cap.get(4)?.as_str().parse().ok()?,
+            },
+        })
+    }
+}
+
+pub enum Direction {
     Up,
     Down,
     Left,
@@ -52,7 +75,7 @@ enum Direction {
 
 impl Direction {
     /// Difference in (x, y) tuple
-    fn vector(&self, length: usize) -> (isize, isize) {
+    pub fn vector(&self, length: usize) -> (isize, isize) {
         let length = length as isize;
         match self {
             Direction::Up => (-length, 0),
@@ -61,10 +84,27 @@ impl Direction {
             Direction::Right => (0, length),
         }
     }
+
+    pub fn card() -> [Direction; 4] {
+        [
+            Direction::Up,
+            Direction::Down,
+            Direction::Left,
+            Direction::Right,
+        ]
+    }
 }
 
-fn is_corner(x: usize, y: usize) -> bool {
+pub fn is_corner(x: usize, y: usize) -> bool {
     matches!((x, y), (0, 0) | (0, 10) | (10, 0) | (10, 10))
+}
+
+pub fn is_castle(x: usize, y: usize) -> bool {
+    is_corner(x, y) || (x, y) == (5, 5)
+}
+
+pub fn in_board(x: isize, y: isize) -> bool {
+    (0..11).contains(&x) && (0..11).contains(&y)
 }
 
 pub fn possible_moves(board: &Board) -> Vec<Move> {
@@ -75,19 +115,12 @@ pub fn possible_moves(board: &Board) -> Vec<Move> {
         let (curr_x, curr_y) = own_location.to_indices();
         let (curr_x, curr_y) = (curr_x as isize, curr_y as isize);
         let is_king = matches!(board.get(own_location), Some(Piece::King));
-        for dir in [
-            Direction::Up,
-            Direction::Down,
-            Direction::Left,
-            Direction::Right,
-        ]
-        .iter()
-        {
+        for dir in Direction::card().iter() {
             for length in 1..=10 {
                 let (diff_x, diff_y) = dir.vector(length);
                 let new_x = curr_x + diff_x;
                 let new_y = curr_y + diff_y;
-                if !(0..11).contains(&new_x) || !(0..11).contains(&new_y) {
+                if !in_board(new_x, new_y) {
                     // Reached the edge of the board in this direction
                     break;
                 }
@@ -122,7 +155,7 @@ pub fn possible_moves(board: &Board) -> Vec<Move> {
 mod tests {
 
     use super::*;
-    use crate::types::Hnfen;
+    use crate::types::*;
 
     #[test]
     fn display() {
@@ -175,22 +208,40 @@ mod tests {
         assert_eq!(black_moves.len(), 116);
         assert_eq!(white_moves.len(), 60);
 
-        let board = Board::from_hnfen("11/11/11/11/11/11/11/11/11/11/11");
+        let board = Board::from_hnfen("11/11/11/11/11/11/11/11/11/11/11").unwrap();
         assert_eq!(possible_moves(&board).len(), 0);
 
-        let board = Board::from_hnfen("h10/11/11/11/11/11/11/11/11/11/11 h");
+        let board = Board::from_hnfen("h10/11/11/11/11/11/11/11/11/11/11 h").unwrap();
         assert_eq!(possible_moves(&board).len(), 20 - 2); // -2 bc of corners
 
-        let board = Board::from_hnfen("a10/11/11/11/11/11/11/11/11/11/11");
+        let board = Board::from_hnfen("a10/11/11/11/11/11/11/11/11/11/11").unwrap();
         assert_eq!(possible_moves(&board).len(), 20 - 2); // -2 bc of corners
 
-        let board = Board::from_hnfen("11/11/11/11/h10/a10/h10/11/11/11/11");
+        let board = Board::from_hnfen("11/11/11/11/h10/a10/h10/11/11/11/11").unwrap();
         assert_eq!(possible_moves(&board).len(), 10 - 1); // -1 bc of center castle
 
-        let board = Board::from_hnfen("11/11/11/11/a10/h10/a10/11/11/11/11 h");
+        let board = Board::from_hnfen("11/11/11/11/a10/h10/a10/11/11/11/11 h").unwrap();
         assert_eq!(possible_moves(&board).len(), 10 - 1); // -1 bc of center castle
 
-        let board = Board::from_hnfen("11/11/11/11/a10/K10/a10/11/11/11/11 h");
+        let board = Board::from_hnfen("11/11/11/11/a10/K10/a10/11/11/11/11 h").unwrap();
         assert_eq!(possible_moves(&board).len(), 10);
+    }
+
+    #[test]
+    fn moves_hnfen() {
+        let ex_move = Move {
+            from: Position {
+                rank: 11,
+                column: 'a',
+            },
+            to: Position {
+                rank: 1,
+                column: 'b',
+            },
+        };
+        let ex_move_fen = "a11b1";
+
+        assert_eq!(ex_move.as_hnfen(), ex_move_fen);
+        assert_eq!(Move::from_hnfen(ex_move_fen).unwrap(), ex_move);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,15 @@
 use std::convert::TryInto;
 
-use crate::moves::Position;
+use crate::moves::{in_board, is_castle, is_corner, Direction, Move, Position};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Board {
     pub ranks: [Rank; 11],
     pub next: Player,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Rank {
     pub fields: [Option<Piece>; 11],
 }
@@ -18,13 +19,13 @@ const BLACK: &str = "a";
 const KING: &str = "K";
 const RANK_SEP: &str = "/";
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Piece {
     Normal(Player),
     King,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Player {
     /// Starts, is attacker
     Black,
@@ -32,9 +33,9 @@ pub enum Player {
     White,
 }
 
-pub trait Hnfen {
+pub trait Hnfen: Sized {
     fn as_hnfen(&self) -> String;
-    fn from_hnfen(hnfen: &str) -> Self;
+    fn from_hnfen(hnfen: &str) -> Option<Self>;
 }
 
 impl Hnfen for Player {
@@ -46,11 +47,20 @@ impl Hnfen for Player {
         .to_string()
     }
 
-    fn from_hnfen(hnfen: &str) -> Self {
+    fn from_hnfen(hnfen: &str) -> Option<Self> {
         match hnfen {
-            BLACK => Player::Black,
-            WHITE => Player::White,
-            _ => panic!("Unexpected input character: {}", hnfen),
+            BLACK => Some(Player::Black),
+            WHITE => Some(Player::White),
+            _ => None,
+        }
+    }
+}
+
+impl Player {
+    pub fn opposite(&self) -> Player {
+        match self {
+            Player::Black => Player::White,
+            Player::White => Player::Black,
         }
     }
 }
@@ -65,12 +75,21 @@ impl Hnfen for Piece {
         .to_owned()
     }
 
-    fn from_hnfen(hnfen: &str) -> Self {
-        match hnfen {
+    fn from_hnfen(hnfen: &str) -> Option<Self> {
+        Some(match hnfen {
             BLACK => Piece::Normal(Player::Black),
             WHITE => Piece::Normal(Player::White),
             KING => Piece::King,
-            _ => panic!("Unexpected input character: {}", hnfen),
+            _ => return None,
+        })
+    }
+}
+
+impl Piece {
+    pub fn color(&self) -> Player {
+        match self {
+            Piece::Normal(c) => *c,
+            Piece::King => Player::White,
         }
     }
 }
@@ -96,7 +115,7 @@ impl Hnfen for Rank {
         buf
     }
 
-    fn from_hnfen(hnfen: &str) -> Self {
+    fn from_hnfen(hnfen: &str) -> Option<Self> {
         // NOTE this is when I realized that using multi-digit numbers makes the language context-sensitive.
         let mut rank = Rank { fields: [None; 11] };
 
@@ -116,7 +135,7 @@ impl Hnfen for Rank {
                         groups.push(C::Number(k.to_digit(10).unwrap() as usize))
                     }
                 }
-                _ => groups.push(C::Character(Piece::from_hnfen(&k.to_string()))),
+                _ => groups.push(C::Character(Piece::from_hnfen(&k.to_string())?)),
             };
         }
 
@@ -134,10 +153,10 @@ impl Hnfen for Rank {
         }
 
         if c_index != 11 {
-            panic!("Rank not exactly filled by hnfen: {}", hnfen);
+            None
+        } else {
+            Some(rank)
         }
-
-        rank
     }
 }
 
@@ -147,10 +166,28 @@ impl Default for Rank {
     }
 }
 
+impl Rank {
+    pub fn pretty(&self) -> String {
+        let mut buf = String::new();
+        for f in self.fields.iter() {
+            match f {
+                Some(p) => buf.push_str(&p.as_hnfen()),
+                None => buf.push(' '),
+            }
+        }
+        buf
+    }
+}
+
 impl Board {
     pub fn get(&self, pos: &Position) -> Option<Piece> {
         let (x, y) = pos.to_indices();
         self.ranks[y].fields[x]
+    }
+
+    pub fn set(&mut self, pos: &Position, piece: &Option<Piece>) {
+        let (x, y) = pos.to_indices();
+        self.ranks[y].fields[x] = *piece;
     }
 
     pub fn pieces(&self, color: Player) -> Vec<Position> {
@@ -167,6 +204,136 @@ impl Board {
             }
         }
         pos
+    }
+
+    pub fn king(&self) -> Option<Position> {
+        for (y, rank) in self.ranks.iter().enumerate() {
+            for (x, piece) in rank.fields.iter().enumerate() {
+                if let Some(Piece::King) = piece {
+                    return Some(Position::from_indices(x, y));
+                }
+            }
+        }
+        None
+    }
+
+    pub fn king_escaped(&self) -> bool {
+        if let Some(pos) = self.king() {
+            let (x, y) = pos.to_indices();
+            is_corner(x, y)
+        } else {
+            false
+        }
+    }
+
+    /// Returns true if a king at position pos *would* be captured
+    pub fn is_king_capture(&self, pos: &Position) -> bool {
+        //println!("Potential king capture with board\n{}", self.pretty());
+
+        let pos = pos.to_indices();
+        for dir in Direction::card().iter() {
+            let dir_diff = dir.vector(1);
+            let check_place = (pos.0 as isize + dir_diff.0, pos.1 as isize + dir_diff.1);
+
+            if !in_board(check_place.0, check_place.1) {
+                return false; // not captured
+            }
+
+            let check_place = (check_place.0 as usize, check_place.1 as usize);
+            if is_castle(check_place.0, check_place.1) {
+                continue; // captured in this direction
+            }
+
+            if let Some(p) = self.get(&Position::from_indices(check_place.0, check_place.1)) {
+                if p.color() == Player::White {
+                    return false; // not captured
+                } else {
+                    continue; // captured in this direction
+                }
+            } else {
+                return false; // not captured
+            }
+        }
+
+        true
+    }
+
+    pub fn apply(&mut self, mov: &Move) {
+        let (x, y) = mov.from.to_indices();
+        let piece = if let Some(p) = self.ranks[y].fields[x] {
+            p
+        } else {
+            // Probably a nop move
+            return;
+        };
+        let move_color = piece.color();
+        self.ranks[y].fields[x] = None;
+        let (x, y) = mov.to.to_indices();
+        self.ranks[y].fields[x] = Some(piece);
+
+        for dir in Direction::card().iter() {
+            let dir_diff = dir.vector(1);
+            let check_place = (x as isize + dir_diff.0, y as isize + dir_diff.1);
+            if !in_board(check_place.0, check_place.1) {
+                continue;
+            }
+            let other_place = (check_place.0 as usize, check_place.1 as usize);
+            let other_is_king =
+                match self.get(&Position::from_indices(other_place.0, other_place.1)) {
+                    Some(Piece::Normal(c)) if c != move_color => {
+                        // Potential take of other_piece
+                        false
+                    }
+                    Some(Piece::King) if move_color == Player::Black => {
+                        // Potential take of king!
+                        true
+                    }
+                    _ => {
+                        // Nothing here to take, continue with next direction
+                        continue;
+                    }
+                };
+            let opposite_place = (
+                other_place.0 as isize + dir_diff.0,
+                other_place.1 as isize + dir_diff.1,
+            );
+            if !in_board(opposite_place.0, opposite_place.1) {
+                continue;
+            }
+            let opposite_place = (opposite_place.0 as usize, opposite_place.1 as usize);
+
+            if other_is_king {
+                if self.is_king_capture(&Position::from_indices(other_place.0, other_place.1)) {
+                    // Took the king, that's pretty cool
+                    self.set(&Position::from_indices(other_place.0, other_place.1), &None);
+                } else {
+                    // Not taking the king
+                    continue;
+                }
+            } else if let Some(p) =
+                self.get(&Position::from_indices(opposite_place.0, opposite_place.1))
+            {
+                // Is surrounded by other piece of move_color
+                if p.color() == move_color {
+                    self.set(&Position::from_indices(other_place.0, other_place.1), &None);
+                }
+            }
+        }
+        self.next = move_color.opposite();
+    }
+
+    pub fn pretty(&self) -> String {
+        let mut pp = "╔═══════════╗\n".to_string();
+        pp.push_str(
+            &self
+                .ranks
+                .iter()
+                .map(|r| format!("║{}║", r.pretty()))
+                .collect::<Vec<String>>()
+                .join("\n"),
+        );
+        pp.push_str("\n╚═══════════╝");
+        pp
     }
 }
 
@@ -186,27 +353,27 @@ impl Hnfen for Board {
         buf
     }
 
-    fn from_hnfen(hnfen: &str) -> Self {
+    fn from_hnfen(hnfen: &str) -> Option<Self> {
         let splits: Vec<&str> = hnfen.split_whitespace().collect();
-        Board {
+        Some(Board {
             ranks: splits[0]
                 .split(RANK_SEP)
                 .map(Rank::from_hnfen)
-                .collect::<Vec<Rank>>()
+                .collect::<Option<Vec<Rank>>>()?
                 .try_into()
                 .unwrap(),
             next: if let Some(s) = splits.get(1) {
-                Player::from_hnfen(s)
+                Player::from_hnfen(s)?
             } else {
                 Player::Black
             },
-        }
+        })
     }
 }
 
 impl Default for Board {
     fn default() -> Self {
-        Board::from_hnfen(crate::DEFAULT_START_HNFEN)
+        Board::from_hnfen(crate::DEFAULT_START_HNFEN).expect("default map should be good")
     }
 }
 
@@ -243,17 +410,19 @@ mod tests {
                 .split(RANK_SEP),
         );
         for case in expected_success_cases.into_iter() {
-            assert_eq!(Rank::from_hnfen(case).as_hnfen(), case);
+            assert_eq!(Rank::from_hnfen(case).unwrap().as_hnfen(), case);
         }
 
-        assert_eq!(Rank::from_hnfen("00011").as_hnfen(), "11");
-        assert_eq!(Rank::from_hnfen("0a09a0").as_hnfen(), "a9a");
+        assert_eq!(Rank::from_hnfen("00011").unwrap().as_hnfen(), "11");
+        assert_eq!(Rank::from_hnfen("0a09a0").unwrap().as_hnfen(), "a9a");
     }
 
     #[test]
     fn test_board_from_hnfen() {
         assert_eq!(
-            Board::from_hnfen(crate::DEFAULT_START_HNFEN).as_hnfen(),
+            Board::from_hnfen(crate::DEFAULT_START_HNFEN)
+                .unwrap()
+                .as_hnfen(),
             crate::DEFAULT_START_HNFEN
         );
     }
